23c217fbd39f9339d13f9796ecf3867fead88d2c
Harjap <harjap1996nov@gmail.com>
2020-05-28 19:50:17 -0700

    testing

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,11 @@
-import React, { Component } from 'react'
-import Grid from './grid.jsx'
-import data from '../data.js'
+import React, { Component } from 'react';
+import Grid from './grid.jsx';
+import data from '../data.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Grid data={data}/>
-      </div>
-    )
-  }
+	render() {
+		return <div>{/* <Grid data={data}/> */}</div>;
+	}
 }
-
 
 export default App;
